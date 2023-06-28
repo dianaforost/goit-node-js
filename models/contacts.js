caded50e585b1d0aba69256df7d8b3b7a990160f
Diana Forostiana <dianaforost@gmail.com>
2023-06-28 13:28:58 +0200

    feat: changed removeContact

--- models/contacts.js
+++ models/contacts.js
@@ -51,13 +51,10 @@ const getContactById = async (contactId) => {
 
 const removeContact = async (contactId) => {
   try {
-    const result = JSON.parse(await fs.readFile(contactsPath, 'utf-8'));
+    const result = await Contact.find();
     const contacts = result.filter((c) => c.id !== contactId);
-    const write = await fs.writeFile(
-      contactsPath,
-      JSON.stringify(contacts),
-      'utf-8'
-    );
+    // const write = await Contact.remove({ _id: contactId });
+    const write = await Contact.deleteOne({ _id: contactId });
     console.log(contacts, write);
     return contacts;
   } catch (e) {
