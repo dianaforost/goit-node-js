b291e2f88a3f377caea1c661c980466053f8cd24
Diana Forostiana <dianaforost@gmail.com>
2023-07-04 12:20:37 +0200

    feat: added usersSchema & changed contactSchema

--- controller/contacts.js
+++ controller/contacts.js
@@ -25,9 +25,31 @@ const contactSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+    },
   },
   { versionKey: false }
 );
+const usersSchema = new mongoose.Schema({
+  password: {
+    type: String,
+    required: [true, 'Set password for user'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ['starter', 'pro', 'business'],
+    default: 'starter',
+  },
+  token: String,
+});
+console.log(usersSchema);
 
 const Contact = mongoose.model('Contact', contactSchema);
 const listContacts = async () => {
