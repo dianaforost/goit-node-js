95ac59d04c4ac76fe0cffb13d3b3f946ecaa1894
Diana Forostiana <dianaforost@gmail.com>
2023-07-04 19:27:13 +0200

    feat: loginUser

--- controller/users.js
+++ controller/users.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const { Types } = require('mongoose');
 require('dotenv').config();
-const { DB_HOST } = process.env;
+const { DB_HOST, secret } = process.env;
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 mongoose
   .connect(DB_HOST)
   .then(() => console.log('Database connection successful'))
@@ -54,7 +56,38 @@ const registerUser = async (body) => {
     console.log(e);
   }
 };
+const loginUser = async (body) => {
+  try {
+    const { email, password } = body;
+    if (email && password) {
+      const result = await User.findOne({ email });
+      if (!result) {
+        return { status: 401, message: 'Email or password is wrong' };
+      }
+      const passwordMatch = bcryptjs.compare(password, result.password);
+      if (passwordMatch) {
+        const payload = {
+          id: result.id,
+          email: result.email,
+        };
+        const token = jwt.sign(payload, secret, { expiresIn: '1h' });
+        return {
+          status: 200,
+          token: token,
+          user: { email: email, subscription: result.subscription },
+        };
+      } else {
+        return { status: 401, message: 'Email or password is wrong' };
+      }
+    } else {
+      return { status: 400, message: 'missing required name field' };
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
 module.exports = {
   User,
   registerUser,
+  loginUser,
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -25,4 +25,25 @@ router.post('/register', async (req, res, next) => {
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
+router.post('/login', async (req, res, next) => {
+  try {
+    const { error } = schemas.userSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    const body = req.body;
+    console.log(req.body);
+    const result = await models.loginUser(body);
+    console.log(result);
+
+    res.status(result.status).json(
+      { token: result.token, user: result.user } || {
+        message: result.message,
+      }
+    );
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 module.exports = router;
