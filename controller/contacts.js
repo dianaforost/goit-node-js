fe47c185856dce6f252d844caf1a6552ed84382d
Diana Forostiana <dianaforost@gmail.com>
2023-08-15 15:29:48 +0200

    fix: contacts

--- controller/contacts.js
+++ controller/contacts.js
@@ -29,6 +29,7 @@ const contactSchema = new mongoose.Schema(
     owner: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
+      required: true,
     },
   },
   { versionKey: false }
